f6cfca2b1731c2dca86e10dfda84c2a4594c30fc
tarcizo <tarcizon12@gmail.com>
2023-02-08 09:48:55 -0300

    Tabelas do banco

---
+++ Banco/Cria_Banco.sql
@@ -0,0 +1,25 @@
+create database api_alunos;
+use api_alunos;
+
+CREATE TABLE ALUNO(
+	codigo_aluno INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+    nascimento DATE NOT NULL,
+    sexo VARCHAR(1) NOT NULL
+);
+
+CREATE TABLE ALUNO_HAS_CADEIRA(
+	fk_codigo_cadeira INT NOT NULL,
+	fk_codigo_aluno INT NOT NULL
+);
+
+CREATE TABLE CADEIRA(
+	codigo_cadeira INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL,
+    carga_horaria INT NOT NULL,
+    horario_da_cadeira TIME NOT NULL
+);
+
+
+
+
